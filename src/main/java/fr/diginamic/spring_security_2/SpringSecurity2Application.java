a0bae222a99b06d7a132d3f1bfd643e6baa96b8b
Tommy-BOU <tommy.boubaker@free.fr>
2025-08-28 11:06:51 +0200

    tp 2a rework

--- src/main/java/fr/diginamic/spring_security_2/SpringSecurity2Application.java
+++ src/main/java/fr/diginamic/spring_security_2/SpringSecurity2Application.java
@@ -1,7 +1,5 @@
 package fr.diginamic.spring_security_2;
 
-import org.apache.logging.log4j.message.Message;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -11,65 +9,75 @@ import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.HexFormat;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringSecurity2Application {
 
-	public static BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    public static BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
+    public static List<String> prenoms = new ArrayList<>(List.of("Tommy", "Angeline", "Cyril", "Daris", "Dmitry", "Julien", "Laurence", "Matthieux", "Nuno", "Olivier", "Robin", "Sandrine", "Sarah"));
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
 //		SpringApplication.run(SpringSecurity2Application.class, args);
 
 //		Partie 1
-		System.out.println(getHash("Tommy"));
-
+        for (String prenom : prenoms) {
+            System.out.println(getHash(prenom));
+        }
+        System.out.println("----------------------------------------------------------------------------------------------------------------------");
 //		Partie 2
-		System.out.println(getNeoPrenomHash("Tommy"));
+        for (String prenom : prenoms) {
+            System.out.println(getSmallestNonceForHash(prenom));
+        }
+        System.out.println("----------------------------------------------------------------------------------------------------------------------");
 
 //		Partie 3
-		System.out.println(encoder.encode("Hello world"));
-		String totohaseh = encoder.encode("toto");
-		String newotohaseh = encoder.encode("toto");
-		System.out.println(totohaseh);
-		System.out.println(newotohaseh);
-		System.out.println(encoder.matches("toto", totohaseh));
-		System.out.println(encoder.matches("toto", newotohaseh));
+        System.out.println(encoder.encode("Hello world"));
+        String totohaseh = encoder.encode("toto");
+        String newotohaseh = encoder.encode("toto");
+        System.out.println(totohaseh);
+        System.out.println(newotohaseh);
+        System.out.println(encoder.matches("toto", totohaseh));
+        System.out.println(encoder.matches("toto", newotohaseh));
+        System.out.println("----------------------------------------------------------------------------------------------------------------------");
 
 //		Partie 4
-		String secretKey = "2025";
-		String message = "Diginamic c'est chouette";
+        String secretKey = "2025";
+        String message = "Diginamic c'est chouette";
 
-		SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
 
-		Mac mac = Mac.getInstance("HmacSHA256");
-		mac.init(secretKeySpec);
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(secretKeySpec);
 
-		byte[] hmacBytes = mac.doFinal(message.getBytes());
+        byte[] hmacBytes = mac.doFinal(message.getBytes());
 
-		String hmacBase64 = Base64.getEncoder().encodeToString(hmacBytes);
-		System.out.println("Signature HMAC-SHA256 (Base 64) : " + hmacBase64);
-	}
+        String hmacBase64 = Base64.getEncoder().encodeToString(hmacBytes);
+        System.out.println("Signature HMAC-SHA256 (Base 64) : " + hmacBase64);
+    }
 
-	public static String getHash(String input) throws NoSuchAlgorithmException{
-		MessageDigest digest = MessageDigest.getInstance("SHA-256");
-		byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
-		String hex = HexFormat.of().formatHex(hash);
-		return (hex);
-	}
+    public static String getHash(String input) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+        String hex = HexFormat.of().formatHex(hash);
+        return (hex);
+    }
 
-	public static String getNeoPrenomHash(String input) throws NoSuchAlgorithmException{
-		String neoHash = "";
-		int counter = 0;
-		while (neoHash.isEmpty()){
-			String hash = getHash(input + counter);
-			if (hash.startsWith("0000")){
-				neoHash = hash;
-			}
-			counter++;
-		}
-		return "Le nonce le plus petit qui donne un hash commençant par 0000 pour " + input + " est " + counter + "\n" + neoHash;
-	}
+    public static String getSmallestNonceForHash(String input) throws NoSuchAlgorithmException {
+        String neoHash = "";
+        int counter = 0;
+        while (neoHash.isEmpty()) {
+            String hash = getHash(input + counter);
+            if (hash.startsWith("0000")) {
+                neoHash = hash;
+            }
+            counter++;
+        }
+        return "Le nonce le plus petit qui donne un hash commençant par 0000 pour " + input + " est " + counter + "\n" + neoHash + "\n";
+    }
 
 }
